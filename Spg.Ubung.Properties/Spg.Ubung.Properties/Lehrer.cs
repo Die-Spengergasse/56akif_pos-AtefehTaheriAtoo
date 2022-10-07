86f74b82f4a76be37aec7f17d17d52bb1b85b3ea
AtefehTaheriAtoo <tah210709@spengergasse.at>
2022-10-07 16:13:02 +0200

    Ubung02

---
+++ Spg.Ubung.Properties/Spg.Ubung.Properties/Lehrer.cs
@@ -0,0 +1,40 @@
+﻿namespace ExProperties.App
+{
+    public  class Lehrer
+    {
+        public string Vorname { get;  set; } = String.Empty;
+        public string Zuname { get;  set; } = String.Empty;
+        public string Kuerzel
+        {
+            get {
+                if (Zuname.Length >= 3)
+                { 
+                    return Zuname?.Substring(0, 3).ToUpper() ?? "--";
+                }
+                return "--";
+            }
+        }
+
+        public decimal? Bruttogehalt
+        { get
+            { 
+                return _bruttoGehalt;
+            }
+            
+           
+            set
+            {
+                if (Bruttogehalt != null)
+                {
+                    _bruttoGehalt = value;
+                }
+            }
+        
+        } 
+        private decimal? _bruttoGehalt;
+
+
+        public decimal Nettogehalt => _bruttoGehalt ?? 0 * 0.8M;
+        
+    }
+}
---
+++ Spg.Ubung.Properties/Spg.Ubung.Properties/Rechteck.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http.Headers;
+
+namespace ExProperties.App
+{
+    internal class Rechteck
+    {
+        public int Laenge
+        {
+            get { return _laenge; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Ungültige Länge");
+                }
+                _laenge = value;
+            }
+        }
+        private int _laenge;    
+    
+
+        private int _breite;
+        public int Breite
+        {
+            get 
+            {
+                return _breite;
+            }
+            set
+            {
+                if (value <0)
+                {
+                    throw new ArgumentException("Ungültige Länge");
+                        }
+                _breite = value;
+
+            }
+        }
+
+
+        public int Flaeche 
+        { 
+            get 
+            {
+                return _laenge * _breite;
+                    
+                    } 
+        }
+    }
+}
